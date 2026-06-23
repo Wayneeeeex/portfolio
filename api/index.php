95312914a312d15f2006f113b806d540c31b216b
Christian Wayne Compasion <wayne@Christians-MacBook-Pro.local>
2026-06-23 13:51:50 +0800

    debug: enable error display on vercel

--- api/index.php
+++ api/index.php
@@ -1,2 +1,10 @@
 <?php
+// Force error reporting and debug mode on for Vercel troubleshooting
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+$_ENV['APP_DEBUG'] = 'true';
+$_ENV['LOG_CHANNEL'] = 'stderr';
+
 require __DIR__ . '/../public/index.php';
