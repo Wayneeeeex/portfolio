7126f16d7577e19883231caa5087d301cfc30548
Wayne <you@example.com>
2026-06-23 18:02:00 +0800

    Fix AppServiceProvider HTTPS issue

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,27 +2,15 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
-    public function register(): void
+    public function boot(): void
     {
-        //
+        if (app()->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
-
-    /**
-     * Bootstrap any application services.
-     */
-    use Illuminate\Support\Facades\URL;
-
-public function boot(): void
-{
-    if (app()->environment('production')) {
-        URL::forceScheme('https');
-    }
-}
 }
